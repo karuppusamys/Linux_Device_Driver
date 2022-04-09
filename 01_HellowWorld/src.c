326233de3badbd34fbea9ee3c5626ed26c59e068
karuppusamys <karuppusamy.kovai@gmail.com>
2022-04-09 10:38:47 +0530

    Function name updated

--- 01_HellowWorld/src.c
+++ 01_HellowWorld/src.c
@@ -1,17 +1,17 @@
 #include <linux/module.h>
 
-static int __init ModuleInit(void) 
+static int __init init_driver(void)
 {
 	printk("Hello world!\n");
 	return 0;
 }
 
-static void __exit ModuleExit(void) 
+static void __exit cleanup_driver(void)
 {
 	printk("Goodbye\n");
 }
-module_init(ModuleInit);
-module_exit(ModuleExit);
+module_init(init_driver);
+module_exit(cleanup_driver);
 
 /* Meta Information */
 MODULE_LICENSE("GPL");
